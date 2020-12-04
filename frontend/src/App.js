fff6a0532a5f25e1ad879ce20c7b2af188964f86
tiagodnzz <tiagodnzz@gamil.com>
2020-12-04 16:26:46 -0300

    subindo testes

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Particles from "react-tsparticles";
+import logo from './logo.svg';
+import './App.css';
+import particlesOptions from "./particles.json";
+
+function App() {
+    return (
+        <div className="App">
+            <Particles options={particlesOptions}/>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+            </header>
+        </div>
+    );
+}
+
+export default App;
